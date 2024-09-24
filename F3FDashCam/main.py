4dd8e4c27b3542f547b2fc5e7710968b1bbd76e2
jomarin38 <joel.marin@neuf.fr>
2024-09-24 21:33:57 +0200

    Garbage collection standalone (#1)
    * not tested
    
    * First try
    
    * Fix refactoring
    
    * Calling super class __init__
    
    * set initial status
    
    * add forgotten while loop
    
    * Implement file rotation (not tested)
    
    * Change default path
    
    * Fix directory size computation
    
    * Print removed files
    
    * Correct garbage collector
    
    * Change filename
    
    * Automatically starts recording
    
    * Set video QUALITY to MEDIUM
    
    * Seems to work on Sylvain's Dashcam2
    
    * Fix state transition
    
    * removing tcp server

--- F3FDashCam/main.py
+++ F3FDashCam/main.py
@@ -1,12 +1,134 @@
+from threading import Thread
+
 from picamera2.encoders import H264Encoder, Quality
 from picamera2 import Picamera2
-import time
 from datetime import datetime
-picam2 = Picamera2()
-picam2.configure(picam2.create_video_configuration())
-encoder = H264Encoder()
-while True:
-    filename = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')+'.h264'
-    picam2.start_recording(encoder, filename, quality=Quality.HIGH)
-    time.sleep(60)
-    picam2.stop_recording()
+import os
+import shutil
+from pathlib import Path
+import time
+import socket
+import json
+
+MEDIA_DIR = '/home/pi/Videos'
+MEDIA_QUOTA = 10* (2**30)
+GARBAGE_COLLECTION_RATIO = 0.25
+
+class GarbageCollector(Thread):
+    def __init__(self, target_space):
+        Thread.__init__(self)
+        self.stopped = False
+        self.target_space = target_space
+
+    def run(self):
+        paths = sorted(Path(MEDIA_DIR).iterdir(), key=os.path.getmtime)
+        saved_space = 0
+        i = 0
+        while i <= len(paths) and saved_space<self.target_space:
+            file_to_remove = str(paths[i])
+            saved_space += os.stat(file_to_remove).st_size
+            i += 1
+            print('Removing {}'.format(file_to_remove))
+            os.remove(file_to_remove)
+
+
+class TimerThread(Thread):
+    def __init__(self, camera_handler):
+        Thread.__init__(self)
+        self.stopped = False
+        self.camera_handler = camera_handler
+
+    def ignore_event(self):
+        self.stopped = True
+
+    def run(self):
+        time.sleep(5 * 60)
+        if not self.stopped:
+            self.camera_handler.sequence_timeout()
+
+import socketserver
+
+class tcpClient_Status():
+    Init = 0
+    Listen = 1
+    Accepted = 2
+    Connected = 3
+    InProgress = 4
+    Close = 5
+
+class DashcamTCPClient(Thread):
+    """
+    The request handler class for our server.
+
+    It is instantiated once per connection to the server, and must
+    override the handle() method to implement communication to the
+    client.
+    """
+
+    def __init__(self, server_ip, server_port):
+        self.picam2 = Picamera2()
+        self.picam2.configure(self.picam2.create_video_configuration())
+        self.encoder = H264Encoder()
+        self.running = False
+        self.stop = False
+        self.server_ip = server_ip
+        self.server_port = server_port
+        self.__debug = True
+        self.timer_thread = None
+        self.status = tcpClient_Status.Init
+        super().__init__()
+
+    def run(self):
+        self.start_anonymous_recording()
+
+    def sequence_timeout(self):
+        self._stop_recording()
+        self.start_anonymous_recording()
+
+    def _stop_recording(self):
+        if self.running:
+            self.picam2.stop_recording()
+            self.running = False
+
+    def start_anonymous_recording(self):
+        filename = datetime.now().strftime('%Y-%m-%d-%H-%M-%S') + '.h264'
+        self._start_recording(os.path.join(MEDIA_DIR, filename))
+
+
+    def start_recording_with_context(self, pilot_name, round_number):
+        filename = '{}_{}_{}.h264'.format(str(round_number),
+                                          '_'.join(pilot_name.split()),
+                                          datetime.now().strftime('%Y-%m-%d-%H-%M-%S'))
+        self._start_recording(os.path.join(MEDIA_DIR, filename))
+
+
+    def _start_recording(self, filename):
+        used = self._get_dir_size(MEDIA_DIR)
+        if used > MEDIA_QUOTA:
+            print('Usage : {}, Quota : {}'.format(used, MEDIA_QUOTA))
+            garbage_collector_thread = GarbageCollector(int(MEDIA_QUOTA*GARBAGE_COLLECTION_RATIO))
+            garbage_collector_thread.start()
+        if self.timer_thread is not None:
+            self.timer_thread.ignore_event()
+        self._stop_recording()
+        self.running = True
+        self.picam2.start_recording(self.encoder, filename, quality=Quality.MEDIUM)
+        self.timer_thread = TimerThread(self)
+        self.timer_thread.start()
+
+    def _get_dir_size(self, path):
+        total = 0
+        with os.scandir(path) as it:
+            for entry in it:
+                if entry.is_file():
+                    total += entry.stat().st_size
+                elif entry.is_dir():
+                    total += self._get_dir_size(entry.path)
+        return total
+
+if __name__ == "__main__":
+    HOST, PORT = "192.168.1.251", 10000
+
+    tcp_thread = DashcamTCPClient(HOST, PORT)
+    tcp_thread.start()
+    tcp_thread.join()
